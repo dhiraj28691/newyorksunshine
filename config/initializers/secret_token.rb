5659b490afefee995291c107c18aa85a59203fc5
Michael Ray <michaelburtonray@gmail.com>
2014-03-30 16:47:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newyorksunshine::Application.config.secret_key_base = '1fc0659c357abd51fe553d055d6ed2652c3228ba7b31608e35a4026166ac2d59a4d61c386ad90b0f94aad7f30c65052d3ae632cb737fc0c66cea971859bcd27e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Newyorksunshine::Application.config.session_store :cookie_store, key: '_newyorksunshine_session'
