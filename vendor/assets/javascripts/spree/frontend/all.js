85c077ba36e262608c32dd4271ab3c2eebae7cb8
Michael Ray <michaelburtonray@gmail.com>
2014-04-28 14:50:04 -0400

    enabling jquery bc of checkout js logic

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -4,10 +4,10 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-// require jquery
-// require jquery_ujs
+//= require jquery
+//= require jquery_ujs
 
-// require spree/frontend
+//= require spree/frontend
 
 //= require_tree .
 //= require spree/frontend/spree_newyorksunshine
