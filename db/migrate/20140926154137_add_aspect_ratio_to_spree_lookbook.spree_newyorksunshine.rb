bc14f7fb9baf791bc33919545362687a94adcb2e
Michael Ray <michaelburtonray@gmail.com>
2014-09-28 21:21:49 -0400

    removing unused migration

--- db/migrate/20140926154137_add_aspect_ratio_to_spree_lookbook.spree_newyorksunshine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_newyorksunshine (originally 20140926154054)
-class AddAspectRatioToSpreeLookbook < ActiveRecord::Migration
-  def change
-    add_column :spree_lookbooks, :aspect_ratio, :integer
-  end
-end
